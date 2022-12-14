ca59a6cdf88ff31342f717615ae577747acf27fd
Bianca Pelegrini <57107170+biancamip@users.noreply.github.com>
2022-12-14 14:51:34 -0300

    adds execution output logs

--- ex2.c
+++ ex2.c
@@ -40,3 +40,19 @@ int main(void)
 
    return(0);
 }
+
+
+/*
+
+exec output:
+
+aluno@s-73-103-30:~/Downloads/Lab-SysProg/bianca$ ./ex2 
+5783 diz: sou o pai, meu pai é 3709 e meus filhos são 5784 e 5785
+5784 diz: sou o filho 1, meu pai é 2606 (pai) e meu filho é 5787
+5787 diz: sou o filho 3, meu pai é 2606 (filho 1) e meu filho é 5788
+5788 diz: sou o filho 5, meu pai é 2606 (filho 3) e não devo ter filhos
+5785 diz: sou o filho 2, meu pai é 5783 (pai) e meu filho é 5786
+5786 diz: sou o filho 4, meu pai é 2606 (filho 2) e não devo ter filhos
+aluno@s-73-103-30:~/Downloads/Lab-SysProg/bianca$ 
+
+*/
